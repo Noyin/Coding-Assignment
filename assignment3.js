8c609752f3bbb9c3b323282b49026e9bf3ace4fa
noyin <noyin_lawal@hotmail.com>
2018-06-17 16:04:21 -0600

    Changes to code and comments

--- assignment3.js
+++ assignment3.js
@@ -1,27 +1,27 @@
 function findTriplets(array, sum) {
-  // Return immediately if array is null or its length is less than 3
+  // Return false if array is null or its length is less than 3
   if (array === null || array.length < 3) {
-    return 'No triplets';
+    console.log('No triplets');
+    return false;
   }
 
   // To facilitate an optimal solution, the array is sorted in increasing order
   const sortedArray = array.sort((a, b) => a > b);
 
   // Loop through the sorted array
-  for (let index = 0; index < sortedArray.length; index += 1) {
-    // Declare a fixed index i
-    const i = index;
-    // Keep track of elements in subarray (array after index i) with j assigned the starting index;
+  for (let i = 0; i < sortedArray.length; i += 1) {
+    // Keep track of elements in subarray (array after index i) with j assigned the start index;
     let j = i + 1;
-    // k is assigned the ending index;
+    // k is assigned the end index;
     let k = sortedArray.length - 1;
     // Start a while loop which executes while j < k;
     while (j < k) {
       const total = sortedArray[i] + sortedArray[j] + sortedArray[k];
       // If total the sum of the value at indeces i, j, k equals to inputted sum,
-      // return a triplet exists and return immediately
+      // a triplet exists, print the triplet and return true
       if (total === sum) {
-        return `${sortedArray[i]}, ${sortedArray[j]}, ${sortedArray[k]}`;
+        console.log(`${sortedArray[i]}, ${sortedArray[j]}, ${sortedArray[k]}`);
+        return true;
       } else if (total < sum) {
         // If total the sum of the value at indeces i, j, k is less than the inputted sum,
         // increase index j by 1;
@@ -34,8 +34,9 @@ function findTriplets(array, sum) {
     }
   }
 
-  // Return if no triplets are found
-  return 'No triplets';
+  // Return false if no triplets are found
+  console.log('No triplets');
+  return false;
 }
 
 module.exports = findTriplets;
--- test/assignment3.test.js
+++ test/assignment3.test.js
@@ -1,32 +1,32 @@
 const findTriplets = require('../assignment3');
 
 describe('findTriplets', () => {
-  it('should return the first triplets whose sum is equals to the inputted sum', () => {
-    expect(findTriplets([12, 3, 4, 1, 6, 9, -6], 24)).toBe('3, 9, 12');
+  it('should return true if a triplet\'s sum is equals to the inputted sum', () => {
+    expect(findTriplets([12, 3, 4, 1, 6, 9, -6], 24)).toBe(true);
   });
 });
 
 describe('findTriplets', () => {
-  it('should return the first triplets whose sum is equals to the inputted sum', () => {
-    expect(findTriplets([12, 3, 4, 1, 6, 9, -6, 50, 102], 60)).toBe('1, 9, 50');
+  it('should return true if a triplet\'s sum is equals to the inputted sum', () => {
+    expect(findTriplets([12, 3, 4, 1, 6, 9, -6, 50, 102], 60)).toBe(true);
   });
 });
 
 describe('findTriplets', () => {
-  test('should return \"No triplets\" when there are no triplets whose sum is equals to the inputted sum', () => {
-    expect(findTriplets([13, 3, 4, 1, 6, 9, -6], 24)).toBe('No triplets');
+  test('should return false when there are no triplets whose sum is equals to the inputted sum', () => {
+    expect(findTriplets([13, 3, 4, 1, 6, 9, -6], 24)).toBe(false);
   });
 });
 
 describe('findTriplets', () => {
-  it('should return \"No triplets\" when inputted array is empty', () => {
-    expect(findTriplets([])).toBe('No triplets');
+  it('should return false when inputted array is empty', () => {
+    expect(findTriplets([])).toBe(false);
   });
 });
 
 describe('findTriplets', () => {
-  it('should return \"No triplets\" when inputted array is null', () => {
-    expect(findTriplets(null)).toBe('No triplets');
+  it('should return false when inputted array is null', () => {
+    expect(findTriplets(null)).toBe(false);
   });
 });
 
